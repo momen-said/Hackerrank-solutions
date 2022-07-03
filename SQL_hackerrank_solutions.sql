d932bb313f32d20d2a664064f7eada24d7b444ca
Mo'men Said <79236835+momen-said@users.noreply.github.com>
2022-07-03 11:51:14 +0200

    Update SQL_hackerrank_solutions.sql

--- SQL_hackerrank_solutions.sql
+++ SQL_hackerrank_solutions.sql
@@ -125,3 +125,15 @@ FROM
 (SELECT ROW_NUMBER() OVER (PARTITION BY OCCUPATION ORDER BY NAME)as row_number, * FROM OCCUPATIONS) as temp
 PIVOT
 (MAX(name) FOR OCCUPATION IN ([Doctor], [Professor], [Singer], [Actor])) as pivot1
+-- 43 -- You are given a table, BST, containing two columns: N and P, where N represents the value of a node in Binary Tree, and P is the parent of N.
+/*Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
+Root: If node is root node.
+Leaf: If node is leaf node.
+Inner: If node is neither root nor leaf node.*/
+select case 
+   when P is null then concat(n,' Root')
+   when N in (select distinct p from bst) then concat(n,' Inner')
+   else concat(n,' Leaf')
+end
+from BST order by N
+-- 44 --
