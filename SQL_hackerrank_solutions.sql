52646c083a14e88d5b0730016813060ef264ba88
Mo'men Said <79236835+momen-said@users.noreply.github.com>
2022-07-16 07:03:09 +0200

    Update SQL_hackerrank_solutions.sql

--- SQL_hackerrank_solutions.sql
+++ SQL_hackerrank_solutions.sql
@@ -245,7 +245,19 @@ select min(start_date), max(end_date)
 from CTE_Projects
 group by datediff(day, rownumber, end_date)
 order by DATEDIFF(DAY, MIN(Start_Date), MAX(End_Date)), min(start_date)
--- 51 -- 
-
-
-
+-- 51 -- You are given three tables: Students, Friends and Packages. Students contains two columns: ID and Name. 
+/*Friends contains two columns: ID and Friend_ID (ID of the ONLY best friend). 
+Packages contains two columns: ID and Salary (offered salary in $ thousands per month).
+Write a query to output the names of those students whose best friends got offered a higher salary than them. 
+Names must be ordered by the salary amount offered to the best friends. It is guaranteed that no two students got same salary offer.*/
+select s.name
+from students s
+inner join friends f
+on s.id = f.id
+inner join packages p1
+on s.id = p1.id
+inner join packages p2
+on f.friend_id = p2.id
+where p1.salary < p2.salary
+order by p2.salary
+-- 52 -- 
